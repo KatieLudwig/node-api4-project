11e93d183105dfbcd6bccda27c0da9816b5476e4
Katie Ludwig <katierludwig@gmail.com>
2024-06-30 22:06:39 -0700

    wip

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use(cors());
+
+server.get('api/hello', (req, res) => {
+    res.json({ message: 'api is working'});
+});
+
+server.use('*', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+});
+
+server.use((err, req, res, next) => { // eslint-disabled-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
